8d3f93fc0ecde0f65e8e0cabadfc24c271e59cb0
Bertrand Lanson <bertrand.lanson@protonmail.com>
2024-02-28 19:16:46 +0100

    feat(ci): test matrix generator build

--- tools/version_check.sh
+++ tools/version_check.sh
@@ -15,58 +15,115 @@ function get_keepalived_tags() {
   sort -V -r
 }
 
+# function get_latest_minor_versions() {
+#   local tags=("$@")
+#   local latest_minor_versions=()
+# 
+#   local current_major=""
+#   local current_minor=""
+#   local current_latest=""
+# 
+#   for tag in "${tags[@]}"; do
+#     local major_version=$(echo "$tag" | cut -d. -f1)
+#     local minor_version=$(echo "$tag" | cut -d. -f2)
+# 
+#     if [[ "$major_version.$minor_version" != "$current_major.$current_minor" ]]; then
+#       if [[ -n "$current_major" ]]; then
+#         latest_minor_versions+=("$current_latest")
+#       fi
+#       current_major="$major_version"
+#       current_minor="$minor_version"
+#       current_latest="$tag"
+#     else
+#       local current_patch=$(echo "$current_latest" | cut -d. -f3)
+#       local patch_version=$(echo "$tag" | cut -d. -f3)
+#       if (( $patch_version > $current_patch )); then
+#         current_latest="$tag"
+#       fi
+#     fi
+#   done
+# 
+#   latest_minor_versions+=("$current_latest")
+# 
+#   echo "${latest_minor_versions[@]}"
+# }
+
 function get_latest_minor_versions() {
   local tags=("$@")
-  local latest_minor_versions=()
-
-  local current_major=""
-  local current_minor=""
-  local current_latest=""
+  declare -A latest_minor_versions
 
   for tag in "${tags[@]}"; do
     local major_version=$(echo "$tag" | cut -d. -f1)
     local minor_version=$(echo "$tag" | cut -d. -f2)
+    local patch_version=$(echo "$tag" | cut -d. -f3)
 
-    if [[ "$major_version.$minor_version" != "$current_major.$current_minor" ]]; then
-      if [[ -n "$current_major" ]]; then
-        latest_minor_versions+=("$current_latest")
-      fi
-      current_major="$major_version"
-      current_minor="$minor_version"
-      current_latest="$tag"
+    local current_latest=${latest_minor_versions["$major_version.$minor_version"]}
+    if [[ -z "$current_latest" ]]; then
+      latest_minor_versions["$major_version.$minor_version"]=$tag
     else
       local current_patch=$(echo "$current_latest" | cut -d. -f3)
-      local patch_version=$(echo "$tag" | cut -d. -f3)
-      if (( $patch_version > $current_patch )); then
-        current_latest="$tag"
+      # Compare each component numerically
+      if (( $major_version > $(echo "$current_latest" | cut -d. -f1) )) || \
+         (( $major_version == $(echo "$current_latest" | cut -d. -f1) && $minor_version > $(echo "$current_latest" | cut -d. -f2) )) || \
+         (( $major_version == $(echo "$current_latest" | cut -d. -f1) && $minor_version == $(echo "$current_latest" | cut -d. -f2) && $patch_version > $current_patch )); then
+        latest_minor_versions["$major_version.$minor_version"]=$tag
       fi
     fi
   done
 
-  latest_minor_versions+=("$current_latest")
-
-  echo "${latest_minor_versions[@]}"
+  # Convert the associative array to JSON format
+  local json_output="{"
+  for key in "${!latest_minor_versions[@]}"; do
+    json_output+="\"$key\":\"${latest_minor_versions[$key]}\","
+  done
+  # Remove the trailing comma and close the JSON object
+  json_output="${json_output%,*}}"
+  echo "$json_output"
 }
 
 function get_latest_major_versions() {
   local tags=("$@")
-  local latest_major_versions=()
-  declare -A latest_for_major
+  declare -A latest_major_versions
 
   for tag in "${tags[@]}"; do
     local major_version=$(echo "$tag" | cut -d. -f1)
 
-    if [[ -z ${latest_for_major[$major_version]} || "$tag" > "${latest_for_major[$major_version]}" ]]; then
-      latest_for_major[$major_version]=$tag
+    local current_latest=${latest_major_versions["$major_version"]}
+    if [[ -z "$current_latest" || "$tag" > "$current_latest" ]]; then
+      latest_major_versions["$major_version"]=$tag
     fi
   done
 
-  for version in "${latest_for_major[@]}"; do
-    latest_major_versions+=("$version")
+  # Convert the associative array to JSON format
+  local json_output="{"
+  for key in "${!latest_major_versions[@]}"; do
+    json_output+="\"$key\":\"${latest_major_versions[$key]}\","
   done
-
-  echo "${latest_major_versions[@]}"
+  # Remove the trailing comma and close the JSON object
+  json_output="${json_output%,*}}"
+  echo "$json_output"
 }
+
+
+# function get_latest_major_versions() {
+#   local tags=("$@")
+#   local latest_major_versions=()
+#   declare -A latest_for_major
+# 
+#   for tag in "${tags[@]}"; do
+#     local major_version=$(echo "$tag" | cut -d. -f1)
+# 
+#     if [[ -z ${latest_for_major[$major_version]} || "$tag" > "${latest_for_major[$major_version]}" ]]; then
+#       latest_for_major[$major_version]=$tag
+#     fi
+#   done
+# 
+#   for version in "${latest_for_major[@]}"; do
+#     latest_major_versions+=("$version")
+#   done
+# 
+#   echo "${latest_major_versions[@]}"
+# }
 
 function get_overall_latest_version() {
   local tags=("$@")
@@ -86,22 +143,23 @@ function main() {
 
   # Get the latest version for each minor
   latest_minor_versions=$(get_latest_minor_versions "${tags[@]}")
-  echo "Latest versions for each minor:"
-  for version in ${latest_minor_versions[@]}; do
-
-    echo "Latest for minor $(echo "$version" | cut -d. -f1-2): $version"
-  done
+  echo $latest_minor_versions
+  # for version in ${latest_minor_versions[@]}; do
+  #   echo "Latest for minor $(echo "$version" | cut -d. -f1-2): $version"
+  # done
 
   # Get the latest version for each major
   latest_major_versions=$(get_latest_major_versions "${tags[@]}")
-  echo "Latest versions for each major:"
-  for version in ${latest_major_versions[@]}; do
-    echo "Latest version for major $(echo "$version" | cut -d. -f1): $version"
-  done
+  echo $latest_major_versions
+  # echo "Latest versions for each major:"
+  # for version in ${latest_major_versions[@]}; do
+  #   echo "Latest version for major $(echo "$version" | cut -d. -f1): $version"
+  # done
 
   # Get the overall latest version
   overall_latest_version=$(get_overall_latest_version "${tags[@]}")
-  echo "Overall latest version is: $overall_latest_version"
+  echo $overall_latest_version
+  # echo "Overall latest version is: $overall_latest_version"
 }
 
 main "$@"
