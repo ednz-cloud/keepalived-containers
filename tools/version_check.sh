4362dd1b03231a24f5a79a7fbeccaa966713b4cb
Bertrand Lanson <bertrand.lanson@protonmail.com>
2024-07-15 20:18:58 +0200

    feat: add 2.3.X release, make version checker more flexible

--- tools/version_check.sh
+++ tools/version_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 EXCLUDED_TAGS="2.2.0 2.2.1 2.2.2 2.2.5 2.2.6"
-REGEX_VERSION="^2\.2\.[0-9]+"
+REGEX_VERSION="^2\.(2|[3-9]|[1-9][0-9])\.[0-9]+$"
 
 function get_keepalived_tags() {
   curl --silent https://api.github.com/repos/acassen/keepalived/tags?per_page=100 | \
